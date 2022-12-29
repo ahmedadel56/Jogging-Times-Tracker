6e935652e5573c53525ec45cd4dadfb34ed4335a
ahmedadel56 <ahmdadl563@gmail.com>
2022-12-29 19:59:01 +0200

    Add data validations

--- app/models/jogging_time.rb
+++ app/models/jogging_time.rb
@@ -1,3 +1,5 @@
 class JoggingTime < ApplicationRecord
-  belongs_to :user
+    belongs_to :user
+    validates :minutes, :distance, :date, presence: true
+    validates :minutes, :distance, length: { minimum: 1 }
 end
